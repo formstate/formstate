b77f99f2961eae175a94f09fbd9d7f574e53a101
basarat <basaratali@gmail.com>
2016-12-14 01:06:49 +1100

    more

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,8 @@
 self.$config = {
   title: 'A general purpose FormState for MobX',
+  repo: 'basarat/formstate',
+  twitter: 'basarat',
+  'edit-link': 'https://github.com/basarat/formstate/blob/master/docs',
   nav: [
     // homepage
     {title: 'Home', path: '/'},
