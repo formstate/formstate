ba56f7474bd18ba589b0df4b5c6ec704f508adb1
basarat <basaratali@gmail.com>
2016-12-17 19:05:07 +1100

    nesting test

--- src/tests/formState/validation.ts
+++ src/tests/formState/validation.ts
@@ -28,8 +28,42 @@ describe("FormState validation", () => {
     const res = await form.validate();
     assert.equal(res.hasError, true);
     assert.equal(form.hasError, true);
-    assert.equal(form.items.name.error, 'value required');
     assert.equal(form.error, 'value required');
+    assert.equal(form.items.name.error, 'value required');
+  });
+
+  it("should validate a nested - nested FieldState and pass if valid", async () => {
+    const name = new FieldState({
+      value: '',
+    });
+    const form = new FormState({
+      name: new FormState({
+        name
+      })
+    });
+    const res = await form.validate();
+    assert.equal(res.hasError, false);
+    assert.equal(form.hasError, false);
+  });
+
+  it("should validate a nested - nested FieldState and fail if invalid", async () => {
+    const name = new FieldState({
+      value: '',
+      validators: [
+        (val) => !val && 'value required'
+      ]
+    });
+    const form = new FormState({
+      name: new FormState({
+        name
+      })
+    });
+    const res = await form.validate();
+    assert.equal(res.hasError, true);
+    assert.equal(form.hasError, true);
+    assert.equal(form.error, 'value required');
+    assert.equal(form.items.name.error, 'value required');
+    assert.equal(form.items.name.safeValue.name, '');
   });
 });
 
