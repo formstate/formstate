bf948debd6c5098948b537296f13d82f47b1a9cd
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-02-09 11:46:16 +1100

    form state lazy local validations

--- src/core/formStateLazy.ts
+++ src/core/formStateLazy.ts
@@ -18,24 +18,104 @@ export class FormStateLazy<TValue extends ValidatableArray> implements Validatab
 
   @observable validating = false;
 
-  @action validate() {
+  private _validators: Validator<TValue>[] = [];
+  @action validators = (...validators: Validator<TValue>[]) => {
+    this._validators = validators;
+    return this;
+  }
+
+  @action async validate(): Promise<{ hasError: true } | { hasError: false, value: TValue }> {
     this.validating = true;
-    return Promise.all(this.getFields().map((value) => value.validate())).then(action((_) => {
+    const values = this.getFields();
+    let fieldsResult = await Promise.all(values.map((value) => value.validate()));
+    const done = runInAction(() => {
+      if (fieldsResult.some(f => f.hasError)) {
+        this.validating = false;
+        return true;
+      }
+      return false
+    });
+    if (done) return { hasError: true as true };
+
+    /** Otherwise do any local validations */
+    const error = await applyValidators(this.$, this._validators || []);
+    const res = runInAction(() => {
+      if (error != this._error) {
+        this._error = error;
+      }
       this.validating = false;
-      return { hasError: this.hasError };
-    }));
+
+      const hasError = !!error;
+      if (hasError) {
+        return { hasError: true as true };
+      }
+      return { hasError: false as false, value: this.$ };
+    });
+
+    return res;
   }
 
   @action enableAutoValidation = () => {
     this.getFields().forEach(x => x.enableAutoValidation());
   }
 
+
+  @observable private _error: string | null | undefined = '';
+
+  /**
+   * Does any field or form have an error
+   */
   @computed get hasError() {
+    return this.hasFieldError || this.hasFormError;
+  }
+
+  /**
+   * Does any field have an error
+   */
+  @computed get hasFieldError() {
     return this.getFields().some(f => f.hasError);
   }
 
-  @computed get error() {
+  /**
+   * Does form level validation have an error
+   */
+  @computed get hasFormError() {
+    return !!this._error;
+  }
+
+  /**
+   * Call it when you are `reinit`ing child fields
+   */
+  @action clearFormError() {
+    this._error = '';
+  }
+
+  /**
+   * Error from some sub field if any
+   */
+  @computed get fieldError() {
     const subItemWithError = this.getFields().find(f => !!f.hasError);
-    return subItemWithError == undefined ? undefined : subItemWithError.error;
+    return subItemWithError ? subItemWithError.error : null;
+  }
+
+  /**
+   * Error from form if any
+   */
+  @computed get formError() {
+    return this._error;
+  }
+
+  /**
+   * The first error from any sub (if any) or form error
+   */
+  @computed get error() {
+    return this.fieldError || this.formError;
+  }
+
+  /**
+   * You should only show the form error if there are no field errors
+   */
+  @computed get showFormError() {
+    return !this.hasFieldError && this.hasFormError;
   }
 }
---
+++ src/tests/formStateLazy/localValidations.ts
@@ -0,0 +1,23 @@
+import { FieldState, FormStateLazy } from '../../index';
+import * as assert from 'assert';
+import { delay } from '../utils';
+import { useStrict } from 'mobx';
+
+useStrict(true);
+
+describe("FormStateLazy local validations", () => {
+  it("should validate a local validation", async () => {
+    const name = new FieldState({
+      value: '',
+    });
+    const form = new FormStateLazy(()=>[
+      name,
+    ]).validators(($) => {
+      return $.length < 2 && 'You must have at least two names';
+    });
+    const res = await form.validate();
+    assert.equal(res.hasError, true);
+    assert.equal(form.hasError, true);
+    assert.equal(form.error, 'You must have at least two names');
+  });
+});
