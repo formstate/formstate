69ecd49fe811093129d0d05bf173a528b9cb0005
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-02-09 11:19:42 +1100

    cleanup formstate lazy tests

--- src/tests/formStateLazy/validation.ts
+++ src/tests/formStateLazy/validation.ts
@@ -10,18 +10,6 @@ describe("FormStateLazy validation", () => {
     const name = new FieldState({
       value: '',
     });
-    const form = new FormState({
-      name,
-    });
-    const res = await form.validate();
-    assert.equal(res.hasError, false);
-    assert.equal(form.hasError, false);
-  });
-
-  it("array: should validate a nested FieldState and pass if valid", async () => {
-    const name = new FieldState({
-      value: '',
-    });
     const form = new FormStateLazy(() => [
       name,
     ]);
@@ -34,24 +22,8 @@ describe("FormStateLazy validation", () => {
     const name = new FieldState({
       value: '',
     }).validators(
-        (val) => !val && 'value required'
-    );
-    const form = new FormState({
-      name,
-    });
-    const res = await form.validate();
-    assert.equal(res.hasError, true);
-    assert.equal(form.hasError, true);
-    assert.equal(form.error, 'value required');
-    assert.equal(form.$.name.error, 'value required');
-  });
-
-  it("array: should validate a nested FieldState and fail if invalid", async () => {
-    const name = new FieldState({
-      value: '',
-    }).validators(
-        (val) => !val && 'value required'
-    );
+      (val) => !val && 'value required'
+      );
     const form = new FormStateLazy(() => [
       name,
     ]);
@@ -63,20 +35,6 @@ describe("FormStateLazy validation", () => {
   });
 
   it("should validate a nested - nested FieldState and pass if valid", async () => {
-    const name = new FieldState({
-      value: '',
-    });
-    const form = new FormState({
-      name: new FormState({
-        name
-      })
-    });
-    const res = await form.validate();
-    assert.equal(res.hasError, false);
-    assert.equal(form.hasError, false);
-  });
-
-  it("array: should validate a nested - nested FieldState and pass if valid", async () => {
     const name = new FieldState({
       value: '',
     });
@@ -94,27 +52,8 @@ describe("FormStateLazy validation", () => {
     const name = new FieldState({
       value: '',
     }).validators(
-        (val) => !val && 'value required'
-    );
-    const form = new FormState({
-      name: new FormState({
-        name
-      })
-    });
-    const res = await form.validate();
-    assert.equal(res.hasError, true);
-    assert.equal(form.hasError, true);
-    assert.equal(form.error, 'value required');
-    assert.equal(form.$.name.error, 'value required');
-    assert.equal(form.$.name.$.name.$, '');
-  });
-
-  it("array: should validate a nested - nested FieldState and fail if invalid", async () => {
-    const name = new FieldState({
-      value: '',
-    }).validators(
-        (val) => !val && 'value required'
-    );
+      (val) => !val && 'value required'
+      );
     const form = new FormStateLazy(() => [
       new FormStateLazy(() => [
         name
@@ -126,24 +65,5 @@ describe("FormStateLazy validation", () => {
     assert.equal(form.error, 'value required');
     assert.equal(form.$[0].error, 'value required');
     assert.equal(form.$[0].$[0].$, '');
-  });
-
-  it("dependent validation should work", async () => {
-    const pass1 = new FieldState({ value: '' }).validators((val) => !val && 'Password required');
-    const pass2 = new FieldState({
-      value: '',
-    }).validators((val) => val && val !== pass1.$ && 'Passwords must match');
-    const form = new FormState({
-      pass1,
-      pass2
-    });
-
-    /** Sample user interaction */
-    form.$.pass1.onChange('hello');
-    form.$.pass2.onChange('he');
-
-    const res = await form.validate();
-    assert.equal(res.hasError, true);
-    assert.equal(pass2.error, 'Passwords must match');
   });
 });
